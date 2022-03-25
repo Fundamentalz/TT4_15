18dad6100da5031df55ae68e4678db1b7c7debc5
Tan Teck Xin Michael <michael.tanteckxin97@gmail.com>
2022-03-25 14:43:17 +0800

    updated loan controller

--- backend_spring/src/main/java/sg/dbs/seed/controller/LoanController.java
+++ backend_spring/src/main/java/sg/dbs/seed/controller/LoanController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sg.dbs.seed.model.CustomerLoan;
 import sg.dbs.seed.model.Loan;
 import sg.dbs.seed.repo.CustomerLoanRepo;
 import sg.dbs.seed.repo.LoanRepo;
@@ -24,7 +25,10 @@ public class LoanController {
 
     @GetMapping("/all")
     public List<Loan> findAllLoans(@RequestParam("id") Integer id) {
-        Customer customer =
+        Customer customer = clRepo.findByCustomerId("customer");
+        CustomerLoan customerLoan = clRepo.findByLoanId("id");
+        Loan_Amount loan_amount = lRepo.findByloanamount("loan_amount");
+        return loan_amount;
     }
 
 }
