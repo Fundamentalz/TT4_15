8e9a772788d67aad58560d5c4b82cc0005c00326
Tan Teck Xin Michael <michael.tanteckxin97@gmail.com>
2022-03-25 15:37:37 +0800

    added payment repo

--- backend_spring/src/main/java/sg/dbs/seed/repo/PaymentRepo.java
+++ backend_spring/src/main/java/sg/dbs/seed/repo/PaymentRepo.java
@@ -1,9 +1,21 @@
 package sg.dbs.seed.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
+import sg.dbs.seed.model.Customer;
 import sg.dbs.seed.model.Payment;
 
 @Component
 public interface PaymentRepo extends JpaRepository<Payment, Integer> {
+
+    @Query("SELECT p FROM Payment p WHERE p.id =: id")
+    Payment findByID(@Param("id") Integer id);
+
+    @Query("SELECT p FROM Payment p WHERE p.payment_date =: payment_date")
+    Payment findPaymentDate(@Param("payment_date") String payment_date);
+
+    @Query("SELECT p FROM Payment p WHERE p.payment_amount =: payment_amount")
+    Payment findPaymentAmount(@Param("payment_amount") Double payment_amount);
 }
