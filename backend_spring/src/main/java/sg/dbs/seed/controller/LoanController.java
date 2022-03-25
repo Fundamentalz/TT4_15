df892f8508c9410e0ab8abe0faf3394d7c43e1c7
CY <69207419+kcy-23@users.noreply.github.com>
2022-03-25 13:15:00 +0800

    Added controller and service layer for Customer entity

--- backend_spring/src/main/java/sg/dbs/seed/controller/LoanController.java
+++ backend_spring/src/main/java/sg/dbs/seed/controller/LoanController.java
@@ -1,4 +1,30 @@
 package sg.dbs.seed.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sg.dbs.seed.model.Loan;
+import sg.dbs.seed.repo.CustomerLoanRepo;
+import sg.dbs.seed.repo.LoanRepo;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/loans")
+@CrossOrigin(origins="http://localhost:3000")
 public class LoanController {
+
+    @Autowired
+    LoanRepo lRepo;
+
+    @Autowired
+    CustomerLoanRepo clRepo;
+
+    @GetMapping("/all")
+    public List<Loan> findAllLoans(@RequestParam("id") Integer id) {
+        Customer customer =
+    }
+
 }
